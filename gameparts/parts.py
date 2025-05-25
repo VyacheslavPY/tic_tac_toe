d4dbdfddd3dc306a968b2f3812dcb0d50a740571
Vyacheslav Kuzmin <Kuzmin.Slava81@yandex.ru>
2025-05-25 22:04:13 +0500

    Final Game

--- gameparts/parts.py
+++ gameparts/parts.py
@@ -1,14 +1,44 @@
 class Board:
+    """Класс, который описывает игровое поле."""
+
+    field_size = 3
 
     def __init__(self):
         self.board = [[' ' for _ in range(3)] for _ in range(3)]
 
-    
     def make_move(self, row, col, player):
         self.board[row][col] = player
-
 
     def display(self):
         for row in self.board:
             print('|'.join(row))
             print('_' * 5)
+
+    def is_board_full(self):
+        # Цикл проходится по всем столбцам игрового поля.
+        for i in range(self.field_size):
+            # А потом по всем строчкам.
+            for j in range(self.field_size):
+                # Если находит свободную ячейку...
+                if self.board[i][j] == ' ':
+                    # ... игра продолжается.
+                    return False
+        return True
+
+    # Этот метод будет определять пообеду.
+    def check_win(self, player):
+        # Проверка по горизонталям и вертикалям
+        for i in range(self.field_size):
+            if (all([self.board[i][j] == player for j in range(self.field_size)]) or
+                    all([self.board[j][i] == player for j in range(self.field_size)])):
+                return True
+        if (all([self.board[i][i] == player for i in range(self.field_size)]) or
+                all([self.board[i][self.field_size - 1 - i] == player for i in range(self.field_size)])):
+            return True
+        return False
+
+    def __str__(self):
+        return (
+            'Объект игрового поля размером '
+            f'{self.field_size}x{self.field_size}'
+        )
